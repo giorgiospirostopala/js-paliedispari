33f3946295afa695c2eecbd34ec0f5ac32ff78b9
giorgiospirostopala <gio.topala@gmail.com>
2025-01-20 02:29:45 +0100

    add: 02-pariedispari

--- 02-pariedispari/main.js
+++ 02-pariedispari/main.js
@@ -1,20 +1,51 @@
 /*
 
-Palidroma:
-• Chiedere all’utente di inserire una parola 
-• Creare una funzione per capire se la parola inserita è palindroma
-
 Pari e Dispari:
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
-
-
-Domande da farsi quando si crea una funzione:
-Come dovrebbe chiamarsi?
-Ho bisogno di parametri?
-Devo restituire un valore?
-Se sì, di che tipo?
+L’utente inserisce un numero da 1 a 5 (quindi pari o dispari)
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+Sommiamo i due numeri 
+Stabiliamo se la somma dei due numeri è pari o dispari (usando un'altra funzione)
+Dichiariamo chi ha vinto.
 
 */
 
-// mini task: 
-// 1. 
+// Devo chiedere il numero all'utente.
+let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5:"));
+
+// Gli specifico se ne ha scelto uno pari o dispari.
+if (numeroUtente % 2 === 0) {
+    console.log(`Il numero scelto (${numeroUtente}) è pari.`);
+} else {
+    console.log(`Il numero scelto (${numeroUtente}) è dispari.`);
+}
+
+// Creo la prima funzione
+function functNumeroComputer() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+let numeroComputer = functNumeroComputer();
+console.log(`Il computer ha scelto: ${numeroComputer}`);
+
+// Creo variabile somma dei due numeri
+let somma = numeroUtente + numeroComputer;
+console.log(`La somma dei due numeri è: ${somma}`);
+
+// Tramite una funzione, stabilisco se la somma dei due numeri è pari o dispari
+function verificaPariDispari(x) {
+    if (x % 2 === 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+}
+
+let sommaDeiDueNum = verificaPariDispari(somma);
+console.log(`"${somma}" è un numero ${sommaDeiDueNum}, perciò...`);
+
+// Dichiaro il vincitore
+if ((sommaDeiDueNum === "pari" && numeroUtente % 2 === 0) || (sommaDeiDueNum === "dispari" && numeroUtente % 2 !== 0)) {
+    console.log("Hai vinto!");
+} else {
+    console.log("Ha vinto il computer!");
+}
